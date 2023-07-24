c8d8041d43b70d4eb6c26160e26bd3f26a72c722
daferpe2 <daferpe2@gmail.com>
2023-07-24 12:52:53 -0500

    primer commit para evaluación

---
+++ expresiones_regulares.py
@@ -0,0 +1,22 @@
+# expresión regular 
+import re
+
+pattern = re.compile(r'^([\d]{4,4})\-\d\d-\d\d,(.*),Friendly,.*$')
+
+with open("C:\\Users\\dafer\\Documents\\platzi\\01_curso_expresiones_regulares\\04_archivo\\REGEX-master\\REGEX-master\\files\\results.csv","r",encoding="utf-8") as f:
+    for i in iter(f.readline,""):
+        resultado = re.match(pattern,i)
+        if resultado:
+            print(f"Juego:  {resultado.group(2)}")
+
+    f.close()
+
+# f =open("C:\\Users\\dafer\\Documents\\platzi\\01_curso_expresiones_regulares\\04_archivo\\REGEX-master\\REGEX-master\\files\\results.csv","r",encoding="utf-8")
+
+# for i in f:
+#     res = re.match(pattern,i)
+#     if res:
+#         print(res.group(2))
+
+# f.close()
+
